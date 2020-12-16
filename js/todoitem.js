bbab73612997b64dc16360954717c9718c034121
Mindy DeWaal <kawiititan@gmail.com>
2020-12-15 16:28:52 -0800

    Adding Javascript to create list items and clear them from the list.

---
+++ js/todoitem.js
@@ -0,0 +1,23 @@
+export default class ToDoItem {
+    constructor(id, item){
+        this._id = id;
+        this._item = item;
+    }
+
+
+    getId(){
+        return this._id;
+    }
+
+    setId(id){
+        this._id = id;
+    }
+
+    getItem(){
+        return this._item;
+    }
+
+    setItem(item){
+        this._item = item;
+    }
+}
